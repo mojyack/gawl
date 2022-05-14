d6d941396804bb7a364f432782a563a222a755ae
mojyack <mojyack@gmail.com>
2022-05-14 21:30:54 +0900

    update keyboard test

--- test/keyboard.cpp
+++ test/keyboard.cpp
@@ -18,12 +18,38 @@ class Keysym;
 
 using GawlKeysym = gawl::Gawl<Keysym>;
 
+enum class Modifiers {
+    None    = 0,
+    Shift   = 1 << 0,
+    Lock    = 1 << 1,
+    Control = 1 << 2,
+    Mod1    = 1 << 3,
+    Mod2    = 1 << 4,
+    Mod4    = 1 << 5,
+};
+
+constexpr auto operator|(const Modifiers a, const Modifiers b) -> Modifiers {
+    return static_cast<Modifiers>(static_cast<int>(a) | static_cast<int>(b));
+}
+
 class Keysym {
+  private:
+    auto calc_modifiers(xkb_state* const state) const -> Modifiers {
+        return (xkb_state_mod_name_is_active(state, XKB_MOD_NAME_SHIFT, xkb_state_component(1)) ? Modifiers::Shift : Modifiers::None) |
+               (xkb_state_mod_name_is_active(state, XKB_MOD_NAME_CAPS, xkb_state_component(1)) ? Modifiers::Lock : Modifiers::None) |
+               (xkb_state_mod_name_is_active(state, XKB_MOD_NAME_CTRL, xkb_state_component(1)) ? Modifiers::Control : Modifiers::None) |
+               (xkb_state_mod_name_is_active(state, XKB_MOD_NAME_ALT, xkb_state_component(1)) ? Modifiers::Mod1 : Modifiers::None) |
+               (xkb_state_mod_name_is_active(state, XKB_MOD_NAME_NUM, xkb_state_component(1)) ? Modifiers::Mod2 : Modifiers::None) |
+               (xkb_state_mod_name_is_active(state, XKB_MOD_NAME_LOGO, xkb_state_component(1)) ? Modifiers::Mod4 : Modifiers::None);
+        return Modifiers::None;
+    }
+
   public:
-    auto keysym_callback(const xkb_keysym_t keysym, const gawl::ButtonState state, const gawl::ModifierFlags modifiers) -> void {
+    auto keysym_callback(const xkb_keycode_t keysym, const gawl::ButtonState state, xkb_state* const xkb_state) -> void {
         static auto buffer = std::array<char, 128>();
         xkb_keysym_get_name(keysym, buffer.data(), buffer.size());
-        std::cout << buffer.data() << "(" << keysym << ")" << " " << static_cast<int>(state) << " " << std::bitset<8>(static_cast<int>(modifiers)) << std::endl;
+        std::cout << buffer.data() << "(" << keysym << ")"
+                  << " " << static_cast<int>(state) << " " << std::bitset<8>(static_cast<int>(calc_modifiers(xkb_state))) << std::endl;
     }
     Keysym(GawlKeysym::Window<Keysym>& /*window*/) {}
 };
