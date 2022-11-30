8ecdbbc76ddf710d53dc21d9ced7e7bcb16c48b3
mojyack <mojyack@gmail.com>
2022-11-30 13:15:57 +0900

    update textrender test

--- test/textrender.cpp
+++ test/textrender.cpp
@@ -32,8 +32,24 @@ class Impl {
         font.draw_fit_rect(window, {{0, 200}, {300, 240}}, {0, 0, 0, 1}, "Hello, World!", 0, gawl::Align::Center, gawl::Align::Left);
 
         constexpr auto poem = "雨ニモマケズ\n風ニモマケズ\n雪ニモ夏ノ暑サニモマケヌ\n丈夫ナカラダヲモチ\n慾ハナク\n決シテ瞋ラズ\nイツモシヅカニワラッテヰル\n一日ニ玄米四合ト\n味噌ト少シノ野菜ヲタベ\nアラユルコトヲ\nジブンヲカンジョウニ入レズニ\nヨクミキキシワカリ\nソシテワスレズ\n野原ノ松ノ林ノ䕃ノ\n小サナ萱ブキノ小屋ニヰテ\n東ニ病気ノコドモアレバ\n行ッテ看病シテヤリ\n西ニツカレタ母アレバ\n行ッテソノ稲ノ束ヲ負ヒ\n南ニ死ニサウナ人アレバ\n行ッテコハガラナクテモイヽトイヒ\n北ニケンクヮヤソショウガアレバ\nツマラナイカラヤメロトイヒ\nヒデリノトキハナミダヲナガシ\nサムサノナツハオロオロアルキ\nミンナニデクノボートヨバレ\nホメラレモセズ\nクニモサレズ\nサウイフモノニ\nワタシハナリタイ";
-        gawl::draw_rect(window, {{310, 0}, {610, 640}}, {1, 1, 1, 1});
-        font.draw_wrapped(window, {{310, 0}, {610, 640}}, 18, {0, 0, 0, 1}, poem, wrapped_text, 16);
+
+        {
+            const auto area = gawl::Rectangle{{310, 0}, {610, 640}};
+            gawl::draw_rect(window, area, {1, 1, 1, 1});
+            font.draw_wrapped(window, area, 18, {0, 0, 0, 1}, poem, wrapped_text, 16, gawl::Align::Left);
+        }
+
+        {
+            const auto area = gawl::Rectangle{{620, 0}, {920, 640}};
+            gawl::draw_rect(window, area, {1, 1, 1, 1});
+            font.draw_wrapped(window, area, 18, {0, 0, 0, 1}, poem, wrapped_text, 16, gawl::Align::Center);
+        }
+
+        {
+            const auto area = gawl::Rectangle{{930, 0}, {1230, 640}};
+            gawl::draw_rect(window, area, {1, 1, 1, 1});
+            font.draw_wrapped(window, area, 18, {0, 0, 0, 1}, poem, wrapped_text, 16, gawl::Align::Right);
+        }
     }
 
     Impl(Gawl::Window<Impl>& window) : window(window), font({fc::find_fontpath_from_name("Noto Sans CJK JP").data()}, 32) {}
