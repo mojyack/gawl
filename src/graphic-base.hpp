077bcfad7aaaf8686672b2f8ae125b124c07a42f
mojyack <mojyack@gmail.com>
2023-09-05 16:54:01 +0900

    use GL_CLAMP_TO_EDGE instead of GL_CLAMP

--- src/graphic-base.hpp
+++ src/graphic-base.hpp
@@ -88,8 +88,8 @@ class GraphicBase {
         const auto txbinder = bind_texture();
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     }
 
     GraphicBase(GraphicBase&& o) {
