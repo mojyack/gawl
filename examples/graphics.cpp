3fbf00701f8acc2b4081036d89fb9d7cb292dc72
mojyack <mojyack@gmail.com>
2024-05-29 14:58:53 +0900

    examples: restore graphics

--- examples/graphics.cpp
+++ examples/graphics.cpp
@@ -1,26 +1,32 @@
-#include <gawl/graphic.hpp>
-#include <gawl/wayland/gawl.hpp>
+#include "gawl/graphic.hpp"
+#include "gawl/misc.hpp"
+#include "gawl/wayland/application.hpp"
 
-class Impl {
+class Callbacks : public gawl::WindowCallbacks {
   private:
-    gawl::Window<Impl>& window;
-    gawl::Graphic       graphic;
+    gawl::Graphic graphic;
 
   public:
-    auto refresh_callback() -> void {
+    auto refresh() -> void override {
         gawl::clear_screen({0, 0, 0, 1});
-        graphic.draw(window, {0, 0});
-        graphic.draw_fit_rect(window, {{0, 170}, {170, 340}});
-        graphic.draw_rect(window, {{170, 0}, {340, 340}});
-        graphic.draw_transformed(window, {{{510, 0}, {680, 170}, {510, 340}, {340, 170}}});
-        graphic.draw_fit_rect(window, {{680, 0}, {1020, 340}});
+        graphic.draw(*window, {0, 0});
+        graphic.draw_fit_rect(*window, {{0, 170}, {170, 340}});
+        graphic.draw_rect(*window, {{170, 0}, {340, 340}});
+        graphic.draw_transformed(*window, {{{510, 0}, {680, 170}, {510, 340}, {340, 170}}});
+        graphic.draw_fit_rect(*window, {{680, 0}, {1020, 340}});
     }
-    Impl(gawl::Window<Impl>& window) : window(window), graphic(gawl::PixelBuffer::from_file("examples/image.png").unwrap()) {}
+
+    auto close() -> void override {
+        application->quit();
+    }
+
+    Callbacks()
+        : graphic(gawl::PixelBuffer::from_file("examples/image.png").value()) {}
 };
 
 auto main() -> int {
-    auto app = gawl::Application();
-    app.open_window<Impl>({.manual_refresh = false});
+    auto app = gawl::WaylandApplication();
+    app.open_window({}, new Callbacks());
     app.run();
     return 0;
 }
