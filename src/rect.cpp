2c3a7173d4b058bae4ff562e406399d11b74e496
mojyack <mojyack@gmail.com>
2024-05-29 14:32:40 +0900

    split to rect.cpp

---
+++ src/rect.cpp
@@ -0,0 +1,43 @@
+#include "rect.hpp"
+
+namespace gawl {
+auto Rectangle::magnify(const double scale) -> Rectangle& {
+    a.magnify(scale);
+    b.magnify(scale);
+    return *this;
+}
+
+auto Rectangle::expand(const double w, const double h) -> Rectangle& {
+    a.x -= w;
+    a.y -= h;
+    b.x += w;
+    b.y += h;
+    return *this;
+}
+
+auto Rectangle::to_points() const -> std::array<Point, 4> {
+    return {a, {b.x, a.y}, b, {a.x, b.y}};
+}
+
+auto Rectangle::width() const -> double {
+    return b.x - a.x;
+}
+
+auto Rectangle::height() const -> double {
+    return b.y - a.y;
+}
+
+auto Rectangle::operator+=(const Point& o) -> Rectangle& {
+    a += o;
+    b += o;
+    return *this;
+}
+
+auto Rectangle::operator&=(const Rectangle& o) -> Rectangle& {
+    a.x = std::max(a.x, o.a.x);
+    a.y = std::max(a.y, o.a.y);
+    b.x = std::min(b.x, o.b.x);
+    b.y = std::min(b.y, o.b.y);
+    return *this;
+}
+} // namespace gawl
--- src/rect.hpp
+++ src/rect.hpp
@@ -8,44 +8,12 @@ struct Rectangle {
     Point a;
     Point b;
 
-    auto magnify(const double scale) -> Rectangle& {
-        a.magnify(scale);
-        b.magnify(scale);
-        return *this;
-    }
-
-    auto expand(const double w, const double h) -> Rectangle& {
-        a.x -= w;
-        a.y -= h;
-        b.x += w;
-        b.y += h;
-        return *this;
-    }
-
-    auto to_points() const -> std::array<Point, 4> {
-        return {a, {b.x, a.y}, b, {a.x, b.y}};
-    }
-
-    auto width() const -> double {
-        return b.x - a.x;
-    }
-
-    auto height() const -> double {
-        return b.y - a.y;
-    }
-
-    auto operator+=(const Point& o) -> Rectangle& {
-        a += o;
-        b += o;
-        return *this;
-    }
-
-    auto operator&=(const Rectangle& o) -> Rectangle& {
-        a.x = std::max(a.x, o.a.x);
-        a.y = std::max(a.y, o.a.y);
-        b.x = std::min(b.x, o.b.x);
-        b.y = std::min(b.y, o.b.y);
-        return *this;
-    }
+    auto magnify(double scale) -> Rectangle&;
+    auto expand(double w, double h) -> Rectangle&;
+    auto to_points() const -> std::array<Point, 4>;
+    auto width() const -> double;
+    auto height() const -> double;
+    auto operator+=(const Point& o) -> Rectangle&;
+    auto operator&=(const Rectangle& o) -> Rectangle&;
 };
 } // namespace gawl
