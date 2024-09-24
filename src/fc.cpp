466070c2f43abc401e5a29f2c2794f77ed92c14f
mojyack <mojyack@gmail.com>
2024-09-24 14:51:30 +0900

    fc: cleanup

--- src/fc.cpp
+++ src/fc.cpp
@@ -3,28 +3,26 @@
 
 #include <fontconfig/fontconfig.h>
 
+#include "macros/assert.hpp"
+#include "macros/autoptr.hpp"
+
+namespace {
+declare_autoptr(FcConfig, FcConfig, FcConfigDestroy);
+declare_autoptr(FcPattern, FcPattern, FcPatternDestroy);
+} // namespace
+
 namespace gawl {
 auto find_fontpath_from_name(const char* const name) -> std::optional<std::string> {
-    const auto config  = FcInitLoadConfigAndFonts();
-    const auto pattern = FcNameParse((const FcChar8*)(name));
-    FcConfigSubstitute(config, pattern, FcMatchPattern);
-    FcDefaultSubstitute(pattern);
+    const auto config  = AutoFcConfig(FcInitLoadConfigAndFonts());
+    const auto pattern = AutoFcPattern(FcNameParse((const FcChar8*)(name)));
+    FcConfigSubstitute(config.get(), pattern.get(), FcMatchPattern);
+    FcDefaultSubstitute(pattern.get());
 
-    auto result = FcResult();
-    auto path   = std::string();
-    if(const auto font = FcFontMatch(config, pattern, &result)) {
-        auto file = (FcChar8*)(nullptr);
-        if(FcPatternGetString(font, FC_FILE, 0, &file) == FcResultMatch) {
-            path = std::string((char*)file);
-        }
-        FcPatternDestroy(font);
-    }
-    FcPatternDestroy(pattern);
-    FcConfigDestroy(config);
-    if(path.empty()) {
-        return std::nullopt;
-    } else {
-        return path;
-    }
+    auto       result = FcResult();
+    const auto font   = AutoFcPattern(FcFontMatch(config.get(), pattern.get(), &result));
+    ensure(font);
+    auto file = (FcChar8*)(nullptr);
+    ensure(FcPatternGetString(font.get(), FC_FILE, 0, &file) == FcResultMatch);
+    return (char*)file;
 }
 } // namespace gawl
