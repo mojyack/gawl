42bc5d0f09100be96d467ec3e780d8db32357231
mojyack <mojyack@gmail.com>
2026-06-16 01:59:41 +0900

    color: add parse_color_code

--- src/color.hpp
+++ src/color.hpp
@@ -3,4 +3,13 @@
 
 namespace gawl {
 using Color = std::array<double, 4>;
+
+constexpr auto parse_color_code(uint32_t num) -> Color {
+    auto ret = Color();
+    ret[0]   = (num >> 24 & 0xFF) / 255.0;
+    ret[1]   = (num >> 16 & 0xFF) / 255.0;
+    ret[2]   = (num >> 8 & 0xFF) / 255.0;
+    ret[3]   = (num >> 0 & 0xFF) / 255.0;
+    return ret;
 }
+} // namespace gawl
